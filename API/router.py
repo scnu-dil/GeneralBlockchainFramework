592108d054a6e3c544f0ee668025e8d925cb4fea
shanxuan <781244184@qq.com>
2020-05-25 21:11:48 +0800

    upload interface

--- API/router.py
+++ API/router.py
@@ -5,6 +5,7 @@ import flask_restful as restful
 
 from upload import *
 from hello import *
+from todo import *
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -13,6 +14,7 @@ api = restful.Api(app)
 api.add_resource(HelloWorld, '/helloWorld')
 api.add_resource(Todo, '/todos/<todo_id>')
 api.add_resource(TodoList, '/todos')
+api.add_resource(Upload, '/upload/<item_Element>')
 
 
 
--- API/upload/__init__.py
+++ API/upload/__init__.py
@@ -1,57 +1,44 @@
+
+
+
 import flask_restful as restful
 from flask_restful import reqparse, abort, Api, Resource
 
 parser = reqparse.RequestParser()
 parser.add_argument('task', type=str)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+# item_Element string (key)
+# item_N_line string
+# item_O_XH string
+# item_O_XFe string
+# item_O_loge string
+# item_C_XH string
+# item_C_XFe string
+# item_C_loge string
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
+# get: http://127.0.0.1:5000/upload/AABCC01010
+# put:  http://127.0.0.1:5000/upload/AABCC01010
+#
+#   item_N_line = 101
+#   item_O_XH = 102
+#   item_O_XFe = 103
+#   item_O_loge = 104
+#   item_C_XH = 105
+#   item_C_XFe = 106
+#   item_C_loge = 107
 
-    def put(self, todo_id):
+# Postman
+
+
+class Upload(Resource):
+    def get(self, item_Element):
+        pass
+
+    def put(self, item_Element):
         args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-
-
-
-# TodoList
-#   shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        return TODOS
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-
-
-
-
-
-
+        item_N_line = args['item_N_line']
 
 
 
