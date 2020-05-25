e5cb0080e76a65de6b34d7080ccc4032c7370dd8
linzh17 <15018279123@163.com>
2020-05-25 22:23:31 +0800

    upload2

--- API/upload/__init__.py
+++ API/upload/__init__.py
@@ -5,10 +5,17 @@ import flask_restful as restful
 from flask_restful import reqparse, abort, Api, Resource
 
 parser = reqparse.RequestParser()
-parser.add_argument('task', type=str)
+parser.add_argument('item_N_line', type=str)
+parser.add_argument('item_O_XH', type=str)
+parser.add_argument('item_O_XFe', type=str)
+parser.add_argument('item_O_loge', type=str)
+parser.add_argument('item_C_XH', type=str)
+parser.add_argument('item_C_XFe', type=str)
+parser.add_argument('item_C_loge', type=str)
 
-
-
+ITEMS  = {
+    'AABCC01010':{ 'item_N_line' :'101' ,'item_O_XH':'102','item_O_XFe' :'103','item_O_loge' :'104','item_C_XH' :'105','item_C_XFe':'106','item_C_loge':'107'}
+}
 # item_Element string (key)
 # item_N_line string
 # item_O_XH string
@@ -30,15 +37,37 @@ parser.add_argument('task', type=str)
 #   item_C_loge = 107
 
 # Postman
+def abort_if_item_exist(item_Element):
+    if item_Element not in ITEMS:
+        abort(404, message="Item{} doesn't exist".format(item_Element))
 
+#避免局部修改造成数据值缺失
+def get_args(args,id,key):
+    temp = args.get(key)
+    if temp == '':
+        temp = ITEMS[id][key]
+    return temp
 
 class Upload(Resource):
     def get(self, item_Element):
-        pass
+        abort_if_item_exist(item_Element)
+        return ITEMS[item_Element]
 
     def put(self, item_Element):
+        abort_if_item_exist(item_Element)
         args = parser.parse_args()
-        item_N_line = args['item_N_line']
 
+        item_N_line = get_args(args,item_Element,'item_N_line')
+        item_O_XH = get_args(args,item_Element,'item_O_XH')
+        item_O_XFe = get_args(args,item_Element,'item_O_XFe')
+        item_O_loge = get_args(args,item_Element,'item_O_loge')
+        item_C_XH = get_args(args,item_Element,'item_C_XH')
+        item_C_XFe = get_args(args,item_Element,'item_C_XFe')
+        item_C_loge = get_args(args,item_Element,'item_C_loge')
 
+        new_item_Element = {'item_N_line' :item_N_line ,'item_O_XH':item_O_XH,'item_O_XFe' :item_O_XFe,' item_O_loge' :item_O_XFe,'item_C_XH' :item_C_XH,'item_C_XFe':item_C_XFe,'item_C_loge':item_C_loge}
+
+        ITEMS[item_Element] = new_item_Element
+
+        return new_item_Element,201
 
