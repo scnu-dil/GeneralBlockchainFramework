d2cb36118f5845909dbb7f2a67130fefdc508c8c
shanxuan <781244184@qq.com>
2020-06-02 22:13:19 +0800

    contracts path

--- contracts/moduels/Tianwen.py
+++ contracts/moduels/Tianwen.py
@@ -10,6 +10,7 @@ path = '/root/GeneralBlockchainFramework/contracts/contracts/'
 class Tianwen(object):
     def __init__(self, contract_name = 'TianwenKV', sol_file=path+'TianwenKV.sol', abi_file=path+'TianwenKV.abi', bin_file=path+'TianwenKV.bin'):
 
+        print ("sol_file:", sol_file)
         self.contractManager = ContractManager(sol_file, abi_file, bin_file, DEBUG=True)
         # checkContractExit这个函数出问题了
         result, self.contractAddress = self.contractManager.checkContractExit(contract_name)
