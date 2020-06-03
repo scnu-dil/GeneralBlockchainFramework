ca41ba8cc5a57d3d0951d48264da4d29dd6c4c3a
shanxuan <781244184@qq.com>
2020-06-03 21:04:56 +0800

    add tianwen

--- API/upload/__init__.py
+++ API/upload/__init__.py
@@ -20,7 +20,6 @@ ITEMS  = {
     'AABCC01010':{ 'item_N_line' :'101' ,'item_O_XH':'102','item_O_XFe' :'103','item_O_loge' :'104','item_C_XH' :'105','item_C_XFe':'106','item_C_loge':'107'}
 }
 
-tianwen = Tianwen()
 # contractFactory = ContractFactory()
 
 class Upload(Resource):
@@ -60,6 +59,7 @@ class Upload(Resource):
         new_item_Element = {'item_N_line' :item_N_line ,'item_O_XH':item_O_XH,'item_O_XFe' :item_O_XFe,' item_O_loge' :item_O_loge,'item_C_XH' :item_C_XH,'item_C_XFe':item_C_XFe,'item_C_loge':item_C_loge}
         ITEMS[item_Element] = new_item_Element
 
+        tianwen = Tianwen()
         txHash = tianwen.set(item_Element, new_item_Element['item_N_line'], new_item_Element['item_O_XH'], new_item_Element['item_O_XFe'],
                              new_item_Element['item_O_loge'], new_item_Element['item_C_XH'], new_item_Element['item_C_XFe'], new_item_Element['item_C_loge'])
 
