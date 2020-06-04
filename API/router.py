50f5feca883a49eec530b88b751893759773ee66
shanxuan <781244184@qq.com>
2020-06-04 22:13:11 +0800

    修改API路径

--- API/router.py
+++ API/router.py
@@ -9,18 +9,13 @@ from flask import Flask
 import flask_restful as restful
 
 from API.upload import *
-from API.hello import *
-from API.todo import *
 from API.static import *
 
 app = Flask(__name__)
 api = restful.Api(app)
 
 
-api.add_resource(HelloWorld, '/helloWorld')
-api.add_resource(Todo, '/todos/<todo_id>')
-api.add_resource(TodoList, '/todos')
-api.add_resource(Upload, '/upload/<item_Element>')
+api.add_resource(Upload, '/astronomy/upload/<item_Element>')
 
 
 
