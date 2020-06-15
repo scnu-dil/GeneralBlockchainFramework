b1defe37431ae7fbf61a4f2b77dc0884738fb6d4
shanxuan <781244184@qq.com>
2020-06-15 10:54:48 +0800

    add traffic router

--- API/router.py
+++ API/router.py
@@ -9,8 +9,9 @@ from flask import Flask
 import flask_restful as restful
 
 from API.upload import *
-from API.static import *
+from API.static import tianwen, traffic
 from API.blockchain import *
+from API.traffic import Traffic
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -18,6 +19,7 @@ api = restful.Api(app)
 
 api.add_resource(Upload, '/upload/<item_Element>')
 api.add_resource(Blockchain, '/<method>')
+api.add_resource(Traffic, '/<method>')
 
 #  malformed url rule:
 
